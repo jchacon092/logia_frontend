a6dacb68d087ccd340f87db12e95bf5275010719
jchacon092 <jchacon092@hotmail.com>
2026-04-06 09:06:21 -0600

    Cambios se quito credencial quemada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,8 @@ import { Navigate } from 'react-router-dom';
 
 export default function Login(){
   const { user, login } = useAuth();
-  const [email, setEmail] = useState('super@logia.test');
-  const [password, setPassword] = useState('secret');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
